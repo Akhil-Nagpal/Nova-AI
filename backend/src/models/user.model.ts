083a9f3b47e2cf0960fa2947a1f5efd70eb09508
Akhil-Nagpal <akhilnagpal07@gmail.com>
2026-09-01 18:21:59 +0530

    feat(auth): add User model with password hashing and JWT token generation

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt, { type SignOptions } from "jsonwebtoken";
 
 export interface IUser {
   fullName: string;
@@ -10,8 +12,15 @@ export interface IUser {
   createdAt: Date;
   updatedAt: Date;
 }
+export interface IUserMethods {
+  comparePassword(password: string): Promise<boolean>;
+  generateAccessToken(): string;
+  generateRefreshToken(): string;
+}
 
-const userSchema = new mongoose.Schema(
+type UserModel = mongoose.Model<IUser, {}, IUserMethods>;
+
+const userSchema = new mongoose.Schema<IUser, UserModel, IUserMethods>(
   {
     fullName: {
       type: String,
@@ -43,4 +52,38 @@ const userSchema = new mongoose.Schema(
 // Indexes
 userSchema.index({ email: 1 });
 
-export const User = mongoose.model<IUser>("User", userSchema);
+// Adding password hashing using bcrypt
+userSchema.pre("save", async function () {
+  // check if the password modified or not, if not then dont hash
+  if (!this.isModified("password")) return;
+  // if the password is modified then hash the password
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// function for comparing the password, when changing
+userSchema.methods.comparePassword = async function (password: string) {
+  // compare the new password with saved one
+  return await bcrypt.compare(password, this.password);
+};
+
+// adding method for generating access token
+userSchema.methods.generateAccessToken = function () {
+  const payload = { _id: this._id };
+  const secretKey = Bun.env.ACCESS_TOKEN_SECRET!;
+  const options: SignOptions = {
+    expiresIn: Number(Bun.env.ACCESS_TOKEN_EXPIRY),
+  };
+  return jwt.sign(payload, secretKey, options);
+};
+
+// adding method for generating refresh token
+userSchema.methods.generateRefreshToken = function () {
+  const payload = { _id: this._id };
+  const secretKey = Bun.env.REFRESH_TOKEN_SECRET!;
+  const options: SignOptions = {
+    expiresIn: Number(Bun.env.REFRESH_TOKEN_EXPIRY),
+  };
+  return jwt.sign(payload, secretKey, options);
+};
+
+export const User = mongoose.model<IUser, UserModel>("User", userSchema);
