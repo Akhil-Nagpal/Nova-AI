7bc276c01621dfb2ec4f49655de00f805bca16ac
Akhil-Nagpal <akhilnagpal07@gmail.com>
2026-09-02 17:17:11 +0530

    chore: add auth routes skeleton (register, login, logout, refresh)

---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/register");
+
+router.post("/login");
+
+router.post("/logout");
+
+router.post("/refresh-token");
+
+export default router;
